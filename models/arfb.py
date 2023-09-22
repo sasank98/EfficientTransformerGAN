4a6d193434004703f473604be5490db2e97a4f2e
AjaySriram Muthuraman <muthuraman.a@northeastern.edu>
2023-09-22 19:43:31 -0400

    Comments for the arfb & comm file
    Description of the class and functions

--- models/arfb.py
+++ models/arfb.py
@@ -3,21 +3,29 @@ import torch.nn as nn
 
 from .comm import defaultConv
 
-
+# Import defaultConv and other necessary components from the .comm module
 class ResidualUnit(nn.Module):
     def __init__(self, inChannel, outChannel, reScale, kernelSize=1, bias=True):
         super().__init__()
 
+        # Define the reduction and expansion convolution layers
         self.reduction = defaultConv(
             inChannel, outChannel//2, kernelSize, bias)
         self.expansion = defaultConv(
             outChannel//2, inChannel, kernelSize, bias)
+        
+        # Store the scaling factors for residual connections
         self.lamRes = reScale[0]
         self.lamX = reScale[1]
 
     def forward(self, x):
+        # Apply the reduction convolution
         res = self.reduction(x)
+
+        # Scale and apply the expansion convolution
         res = self.lamRes * self.expansion(res)
+
+        # Add the scaled residual to the input
         x = self.lamX * x + res
 
         return x
@@ -26,20 +34,34 @@ class ResidualUnit(nn.Module):
 class ARFB(nn.Module):
     def __init__(self, inChannel, outChannel, reScale):
         super().__init__()
+
+        # Create two ResidualUnit instances for the ARFB module
         self.RU1 = ResidualUnit(inChannel, outChannel, reScale)
         self.RU2 = ResidualUnit(inChannel, outChannel, reScale)
+
+        # Define convolution layers
         self.conv1 = defaultConv(2*inChannel, 2*outChannel, kernelSize=1)
         self.conv3 = defaultConv(2*inChannel, outChannel, kernelSize=3)
+
+        # Store scaling factors for residual connections
         self.lamRes = reScale[0]
         self.lamX = reScale[1]
 
     def forward(self, x):
+        # Apply Residual Units
 
         x_ru1 = self.RU1(x)
         x_ru2 = self.RU2(x_ru1)
+
+        # Concatenate the outputs of the Residual Units
         x_ru = torch.cat((x_ru1, x_ru2), 1)
+
+        # Apply convolution layers
         x_ru = self.conv1(x_ru)
         x_ru = self.conv3(x_ru)
+
+
+        # Scale the output and add to the input
         x_ru = self.lamRes * x_ru
         x = x*self.lamX + x_ru
         return x
--- models/comm.py
+++ models/comm.py
@@ -1,23 +1,39 @@
 import torch.nn as nn
+
+# Function for creating a default convolutional layer
 def defaultConv(inChannels, outChannels, kernelSize, bias=True):
     return nn.Conv2d(
         inChannels, outChannels, kernelSize,
         padding=(kernelSize//2), bias=bias)
 
 
+# Class definition for Squeeze-and-Excitation (SE) Layer
 class SELayer(nn.Module):
     def __init__(self, channel, reduction=16):
         super(SELayer, self).__init__()
+
+        # Adaptive average pooling to reduce spatial dimensions to 1x1
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+
+        # Fully connected layers for SE Block
         self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(channel // reduction, channel, bias=False),
-            nn.Sigmoid()
+            nn.Linear(channel, channel // reduction, bias=False), # Reduce dimensionality
+            nn.ReLU(inplace=True), # ReLU activation
+            nn.Linear(channel // reduction, channel, bias=False), # Restore dimensionality
+            nn.Sigmoid() # Sigmoid activation for scale factors
         )
 
     def forward(self, x):
+
+        # Get dimensions of the input tensor
         b, c, _, _ = x.size()
+
+        # Apply adaptive average pooling to reduce spatial dimensions
         y = self.avg_pool(x).view(b, c)
+
+         # Apply fully connected layers to compute scale factors
         y = self.fc(y).view(b, c, 1, 1)
+
+         # Scale the input tensor element-wise by the computed factors
         return x * y.expand_as(x)
